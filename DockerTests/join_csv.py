1b065ce53372395568fabc8edb3f05ddc527d7d6
StormageddonDarkLordOfAll <57874179+StormageddonDarkLordOfAll@users.noreply.github.com>
2023-12-30 13:31:19 +0100

    data merged together
    merged data form each system for each target , except for BRiQ and Ares

--- DockerTests/join_csv.py
+++ DockerTests/join_csv.py
@@ -19,6 +19,8 @@ def join_standard_csv(foldername,number):
     df6.columns=['ID','dfire']
     df7 = pd.read_csv(foldername+'/R1'+number+'_rna3dcnn.csv',sep="	", header=None)
     df7.columns=['ID','rna3dcnn']
+    df7mc = pd.read_csv(foldername+'/R1'+number+'_rna3dcnnMC.csv',sep="	", header=None)
+    df7mc.columns=['ID','rna3dcnn_mc']
     df8 = pd.read_csv(foldername+'/rsrnasp'+number+'.csv',sep="     ", header=None)
     df8.columns=['ID','rsrnasp']
     # Merge the two dataframes, using _ID column as key
@@ -27,51 +29,55 @@ def join_standard_csv(foldername,number):
     dffinal = pd.merge(dffinal, df5, on = 'ID')
     dffinal = pd.merge(dffinal, df6, on = 'ID')
     dffinal = pd.merge(dffinal, df7, on = 'ID')
+    dffinal = pd.merge(dffinal, df7mc, on = 'ID')
     dffinal = pd.merge(dffinal, df8, on = 'ID')
     # dffinal.set_index('ID', inplace = True)
 
     dffinal.insert(1, "3drna", extracted_col)
     # dffinal = pd.concat([dffinal, extracted_col.rename("3drna")], axis=1)
     # Write it to a new CSV file
-    dffinal.to_csv(foldername+'_merged1.csv')
+    dffinal=left_join_dataframe(dffinal,"lociparse.csv")
+    dffinal.to_csv(foldername+'_merged.csv')
     print(dffinal)
 
-# different approach to merging, helped find an issue
-def join_standard_csv2(foldername,number):
-    df1 = pd.read_csv(foldername+'/3drna'+number+'.csv',sep="     ", header=None)
-    df1.columns=['3drna']
-    extracted_col = df1["3drna"]
-    print(extracted_col)
-    df2 = pd.read_csv(foldername+'/cgrnasdpc_R1'+number+'.csv',sep="     ", header=None)
-    df2.columns=['ID','cgrnaspc']
-    df3 = pd.read_csv(foldername+'/cgrnasdppc_R1'+number+'.csv',sep="     ", header=None)
-    df3.columns=['ID','cgrnasppc']
-    extracted_col3 = df3["cgrnasppc"]
-    df4 = pd.read_csv(foldername+'/cgrnasp_cn_R1'+number+'.csv',sep="     ", header=None)
-    df4.columns=['ID','cgrnasp_cn']
-    extracted_col4 = df4["cgrnasp_cn"]
-    df5 = pd.read_csv(foldername+'/cgrnasp_R1'+number+'.csv',sep="     ", header=None)
-    df5.columns=['ID','cgrnasp']
-    extracted_col5 = df5["cgrnasp"]
-    df6 = pd.read_csv(foldername+'/dfireR1'+number+'.csv',sep=";", header=None)
-    df6.columns=['ID','dfire']
-    extracted_col6 = df6["dfire"]
-    df7 = pd.read_csv(foldername+'/R1'+number+'_rna3dcnn.csv',sep="	", header=None)
-    df7.columns=['ID','rna3dcnn']
-    extracted_col7 = df7["rna3dcnn"]
-    df8 = pd.read_csv(foldername+'/rsrnasp'+number+'.csv',sep="     ", header=None)
-    df8.columns=['ID','rsrnasp']
-    extracted_col8 = df8["rsrnasp"]
-    df2.insert(2, "3drna", extracted_col)
-    df2.insert(2, "cgrnasppc", extracted_col3)
-    df2.insert(2, "cgrnasp_cn", extracted_col4)
-    df2.insert(2, "cgrnasp", extracted_col5)
-    df2.insert(2, "dfire", extracted_col6)
-    df2.insert(2, "rna3dcnn", extracted_col7)
-    df2.insert(2, "rsrnasp", extracted_col8)
 
-    # Write it to a new CSV file
-    df2.to_csv(foldername+'_merged.csv')
-    print(df2)
+def left_join_csv(destination,source):
+    df1 = pd.read_csv(destination)
+    df2 = pd.read_csv(source,sep="	", header=None)
+    df2.columns=['ID','lociPARSE']
+    dffinal = pd.merge(df1, df2, on = 'ID', how='left')
+    dffinal.to_csv(destination)
+    print(dffinal)
+def left_join_dataframe(df,source):
 
-join_standard_csv("r1107","107")
+    df2 = pd.read_csv(source,sep="	", header=None)
+    df2.columns=['ID','lociPARSE']
+    dffinal = pd.merge(df, df2, on = 'ID', how='left')
+    # print(dffinal)
+    return dffinal
+
+# join_standard_csv("r1107","107")
+join_standard_csv("r1108","108")
+join_standard_csv("r1116","116")
+join_standard_csv("r1117","117")
+join_standard_csv("r1126","126")
+join_standard_csv("r1128","128")
+join_standard_csv("r1136","136")
+join_standard_csv("r1138","138")
+join_standard_csv("r1149","149")
+join_standard_csv("r1156","156")
+join_standard_csv("r1189","189")
+join_standard_csv("r1190","190")
+
+# left_join_csv("r1107_merged.csv","lociparse.csv")
+# left_join_csv("r1108_merged.csv","lociparse.csv")
+# left_join_csv("r1116_merged.csv","lociparse.csv")
+# left_join_csv("r1117_merged.csv","lociparse.csv")
+# left_join_csv("r1126_merged.csv","lociparse.csv")
+# left_join_csv("r1128_merged.csv","lociparse.csv")
+# left_join_csv("r1136_merged.csv","lociparse.csv")
+# left_join_csv("r1138_merged.csv","lociparse.csv")
+# left_join_csv("r1149_merged.csv","lociparse.csv")
+# left_join_csv("r1156_merged.csv","lociparse.csv")
+# left_join_csv("r1189_merged.csv","lociparse.csv")
+# left_join_csv("r1190_merged.csv","lociparse.csv")
