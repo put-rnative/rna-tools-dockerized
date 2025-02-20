67fc4f8fb49e3289ebfe383fe59170cfcd63ec5c
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-02-20 10:03:18 +0100

    feat: Add help and argument parsing to scoring-wrapper.py

--- scoring-wrapper.py
+++ scoring-wrapper.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 import argparse
 
-
 SCORING_METHODS = [
     "3dRNAscore",
     "DFIRE",
@@ -18,10 +17,22 @@ SCORING_METHODS = [
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="RNA structure scoring wrapper supporting multiple scoring methods"
+    )
     parser.add_argument(
         "--scoring-method",
         choices=SCORING_METHODS,
         action="append",
         help="Scoring method to use (can be specified multiple times). If none given, all methods will be used.",
     )
+
+    args = parser.parse_args()
+    
+    # Use all methods if none specified
+    methods = args.scoring_method if args.scoring_method else SCORING_METHODS
+    print(f"Using scoring methods: {methods}")
+
+
+if __name__ == "__main__":
+    main()
