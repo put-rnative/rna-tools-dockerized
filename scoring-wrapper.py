8eb9cf2359e6c8aee7bc9bd9946dda3f45cc2310
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-02-20 10:04:19 +0100

    feat: Add support for multiple PDB file arguments with validation

--- scoring-wrapper.py
+++ scoring-wrapper.py
@@ -26,12 +26,25 @@ def main():
         action="append",
         help="Scoring method to use (can be specified multiple times). If none given, all methods will be used.",
     )
+    parser.add_argument(
+        "pdb_files",
+        nargs="+",
+        help="One or more PDB files to score",
+    )
 
     args = parser.parse_args()
 
     # Use all methods if none specified
     methods = args.scoring_method if args.scoring_method else SCORING_METHODS
     print(f"Using scoring methods: {methods}")
+    
+    # Validate PDB files exist
+    import os.path
+    for pdb_file in args.pdb_files:
+        if not os.path.isfile(pdb_file):
+            parser.error(f"PDB file not found: {pdb_file}")
+            
+    print(f"Processing PDB files: {args.pdb_files}")
 
 
 if __name__ == "__main__":
